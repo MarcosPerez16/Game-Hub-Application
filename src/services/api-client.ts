28c6feb3d57d5de06ea710f3d0b6994759f29621
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-07-15 07:42:26 -0700

    added notes to the api-client file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,11 @@
-import axios from "axios";
+//tried to create an .env file for this and store the key but it messed up the entire project
+//can come back and try it again
+
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "c7d311696b5a4ed5ba6d188259fbca0c"
-    }
-})
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: 'c7d311696b5a4ed5ba6d188259fbca0c',
+  },
+});
